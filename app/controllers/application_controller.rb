15bfb1d2a33f86f9eeef3f30cbae479d417aea4a
yaoyizhi <email@yaoadou888>
2017-08-15 06:08:21 +0800

    can change languages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,25 +1,35 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery with: :exception
+  protect_from_forgery with: :exception
 
-    def require_admin
-        unless current_user.admin
-            redirect_to root_path
-            flash[:alert] = 'You are not admin!'
-        end
+  def require_admin
+    unless current_user.admin
+      redirect_to root_path
+      flash[:alert] = 'You are not admin!'
+    end
+  end
+
+  helper_method :current_cart
+
+  def current_cart
+    @current_cart ||= find_cart
+  end
+
+  private
+
+  def find_cart
+    cart = Cart.find_by(id: session[:cart_id])
+    cart = Cart.create if cart.blank?
+    session[:cart_id] = cart.id
+    cart
+  end
+
+  before_action :set_locale
+
+  def set_locale
+    if params[:locale] && I18n.available_locales.include?(params[:locale].to_sym)
+      session[:locale] = params[:locale]
     end
 
-    helper_method :current_cart
-
-    def current_cart
-        @current_cart ||= find_cart
-    end
-
-    private
-
-    def find_cart
-        cart = Cart.find_by(id: session[:cart_id])
-        cart = Cart.create if cart.blank?
-        session[:cart_id] = cart.id
-        cart
-    end
+    I18n.locale = session[:locale] || I18n.default_locale
+  end
 end
